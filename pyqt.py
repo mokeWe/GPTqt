589e23fad64496e5cb7f9930dd3ddd5e5ccffee0
Maika <81923935+mokeWe@users.noreply.github.com>
2022-10-27 19:23:08 -0700

    Update pyqt.py
    Minor changes

--- pyqt.py
+++ pyqt.py
@@ -10,7 +10,7 @@ from PyQt6.QtWidgets import (
     QTextEdit,
     QPushButton,
     QComboBox,
-    QVBoxLayout,
+    QGridLayout,
     QMessageBox,
 )
 from pathlib import Path
@@ -23,85 +23,100 @@ class MainWin(QWidget):
         self.tempAmount = float(0.1)
         self.answerAmount = 1
         self.tokenAmount = 10
-        self.cEngine = "davinci"
+
         self.tempSlide = QSlider(Qt.Orientation.Horizontal)
         self.tokenSlide = QSlider(Qt.Orientation.Horizontal)
         self.amountSlide = QSlider(Qt.Orientation.Horizontal)
-        self.tokenStatus = QLabel("Answers: 1")
+
         self.promptEdit = QTextEdit()
         self.responseBox = QTextEdit()
+
         self.tokenStatus = QLabel("Tokens: 10")
-        self.sendButton = QPushButton("Send prompt")
-        self.tempStatus = QLabel("Temperature: 0.0")
+        self.tempStatus = QLabel("Temp: 0.0")
         self.amountStatus = QLabel("Answers: 1")
-        self.engine = QComboBox()
+        self.engineStatus = QLabel("Engine: ")
         self.finished = QLabel()
+
+        self.sendButton = QPushButton("Send prompt")
+        self.engine = QComboBox()
 
     def init_ui(self):
         _keypath = Path("api-key.txt")
         _keypath.touch(exist_ok=True)
+
         with open("api-key.txt", "r") as h:
             _key = h.readline().strip("\n")
         if not _key.startswith("sk-"):
             QMessageBox.critical(
                 self,
-                "Hey!",
-                "API Key not found, or an invalid API key was provided. Set a " "valid key in api-key.txt!",
+                "Error",
+                "API Key not found, or an invalid API key was provided. Set a "
+                "valid key in api-key.txt!",
             )
             sys.exit()
         openai.api_key = _key
 
         models = openai.Model.list()
 
-        # exclude practically useless models (at least for now)
-        exclude = ["instruct", "similarity", "code", "if", "query", "document", "insert", ":", "text-search"]
+        # exclude useless models
+        exclude = [
+            "instruct",
+            "similarity",
+            "if",
+            "query",
+            "document",
+            "insert",
+            ":",
+            "search",
+        ]
         for model in models.data:
             if not any(x in model.id for x in exclude):
                 self.engine.addItems([str(model.id)])
 
+        self.cEngine = self.engine.currentText()
+
         # connections
         self.engine.currentIndexChanged.connect(self.selection_change)
-        self.tokenSlide.setMinimum(10)
-        self.tokenSlide.setMaximum(2000)
+
+        self.tokenSlide.setRange(10, 1000)
         self.tokenSlide.valueChanged.connect(self.token_value)
-        self.amountSlide.setMinimum(1)
-        self.amountSlide.setMaximum(5)
+
+        self.amountSlide.setRange(1, 5)
         self.amountSlide.valueChanged.connect(self.amount_value)
-        self.tempSlide.setMinimum(0)
-        self.tempSlide.setMaximum(15)
+
+        self.tempSlide.setRange(0, 10)
         self.tempSlide.valueChanged.connect(self.change_value_temp)
+
         self.responseBox.setReadOnly(True)
-        self.sendButton.setToolTip("Submit the prompt")
+
         self.sendButton.clicked.connect(self.send_prompt)
 
         # layout
-        layout = QVBoxLayout()
-        for widget in (
-            self.engine,
-            self.amountStatus,
-            self.amountSlide,
-            self.tokenStatus,
-            self.tokenSlide,
-            self.tempStatus,
-            self.tempSlide,
-            self.promptEdit,
-            self.sendButton,
-            self.responseBox,
-            self.finished,
-        ):
-            layout.addWidget(widget)
-        self.setLayout(layout)
+        l = QGridLayout()
+        l.addWidget(self.tempStatus, 0, 0)
+        l.addWidget(self.tempSlide, 0, 1)
+        l.addWidget(self.tokenStatus, 1, 0)
+        l.addWidget(self.tokenSlide, 1, 1)
+        l.addWidget(self.amountStatus, 2, 0)
+        l.addWidget(self.amountSlide, 2, 1)
+        l.addWidget(self.engineStatus, 3, 0)
+        l.addWidget(self.engine, 3, 1)
+        l.addWidget(self.promptEdit, 4, 0, 1, 2)
+        l.addWidget(self.sendButton, 5, 0, 1, 2)
+        l.addWidget(self.responseBox, 6, 0, 1, 2)
+        l.addWidget(self.finished, 7, 0, 1, 2)
+        self.setLayout(l)
 
     def selection_change(self):
         self.cEngine = self.engine.currentText()
 
     def token_value(self):
         self.tokenAmount = self.tokenSlide.value()
-        self.tokenStatus.setText("Tokens: " + str(self.tokenSlide.value()))
+        self.tokenStatus.setText("Tokens: " + str(self.tokenAmount))
 
     def change_value_temp(self):
         self.tempAmount = self.tempSlide.value() / 10
-        self.tempStatus.setText("Temperature: " + str(self.tempSlide.value() / 10))
+        self.tempStatus.setText("Temp: " + str(self.tempSlide.value() / 10))
 
     def amount_value(self):
         self.answerAmount = self.amountSlide.value()
@@ -120,19 +135,23 @@ class MainWin(QWidget):
         )
         # set text box as response
         self.responseBox.setText("")
-        if int(self.answerAmount) > 0:
-            for i in range(int(self.answerAmount)):
-                self.responseBox.append("[+]----------ANSWER-" + str(i + 1) + "---------")
-                self.responseBox.append(response.choices[i].text + "\n\n\n")
+        for i in range(int(self.answerAmount)):
+            self.responseBox.append("[+] ANSWER " + str(i + 1))
+            self.responseBox.append(response.choices[i].text + "\n\n\n")
 
         # write response text to file
         f = open("Responses.txt", "a+")
-        if int(self.answerAmount) > 0:
-            f.write("Prompt: " + self.promptEdit.toPlainText() + " | Engine: " + self.cEngine + "\n")
-            for i in range(int(self.answerAmount)):
-                f.write("\n[+]----------ANSWER-" + str(i + 1) + "---------\n")
-                f.write(response.choices[i].text + "\n")
-                f.write("[+]--------------------------\n\n")
+        f.write(
+            "Prompt: "
+            + self.promptEdit.toPlainText()
+            + " | Engine: "
+            + self.cEngine
+            + "\n"
+        )
+        for i in range(int(self.answerAmount)):
+            f.write("\n[+]----------ANSWER-" + str(i + 1) + "---------\n")
+            f.write(response.choices[i].text + "\n")
+            f.write("[+]--------------------------\n\n")
         f.close()
         self.finished.setText("[+] Done! Also saved to Responses.txt")
 
